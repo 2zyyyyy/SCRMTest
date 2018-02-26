ace5b6e4131902de17ad1b33784ffc0a62ba7258
2zyyyyy <android4google@163.com>
2018-02-26 08:54:04 +0800

    'push'

--- crm/test_case/models/page_object/shopPage.py
+++ crm/test_case/models/page_object/shopPage.py
@@ -2,6 +2,7 @@ from .base import Page
 from selenium.webdriver.common.by import By
 from time import sleep
 from .changepwdPage import *
+from models.page_object.changepwdPage import ChangePwd
 
 
 class Shop(Page):
@@ -11,7 +12,7 @@ class Shop(Page):
     basicsConfig_loc = (By.XPATH, '//*[@id="side-menu"]/li[8]/a/span[1]')  # 基础配置
     shopConfig_loc = (By.XPATH, '//*[@id="side-menu"]/li[8]/ul/li[1]/a')  # 店铺配置
     arrow_loc = (By.XPATH, '//*[@id="shopInfoFrom"]/div[1]/div/div/span/i')  # 下拉框
-    list = (By.CSS_SELECTOR, 'ul.addSetHidden.cumsetHidden>li')
+    list = (By.CSS_SELECTOR, 'ul.addSetHidden.cumsetHidden>li')  # 下拉列表框数据
     # liTmall_LOC = (By.XPATH, '//*[@id="shopInfoFrom"]/div[1]/div/ul/li[1]')  # 淘宝天猫
     # liJd_LOC = (By.XPATH, '//*[@id="shopInfoFrom"]/div[1]/div/ul/li[2]')  # 京东
     shopName_loc = (By.ID, 'shopname')  # 店铺名称
@@ -19,25 +20,36 @@ class Shop(Page):
     saveBtn_loc = (By.ID, 'saveBtn')  # 保存
 
     # Action
+    def basicConfig(self):
+        self.find_element(*self.basicsConfig_loc).click()
+
+    def shopConfig(self):
+        self.find_element(*self.shopConfig_loc).click()
+
     def pullDown(self):
         self.find_element(*self.arrow_loc).click()
 
     def select_shop(self, number):
-        self.find_element(*self.list)[number].click()  # 0淘宝 1京东 2唯品会
+        self.find_elements(*self.list)[number].click()  # 0淘宝 1京东 2唯品会
 
     def shopName(self, shopName):
+        self.find_element(*self.shopName_loc).clear()
         self.find_element(*self.shopName_loc).send_keys(shopName)
 
     def shopKepperName(self, shopKepperName):
-        self.find_element(*self.shopName_loc).send_keys(shopKepperName)
+        self.find_element(*self.shopKeeper_loc).clear()
+        self.find_element(*self.shopKeeper_loc).send_keys(shopKepperName)
 
     def saveBtn(self):
         self.find_element(*self.saveBtn_loc).click()
 
-    def shopSetting(self, platform, shopName, shopKepper):
-        ChangePwd.switch_frame()
+    def shopSetting(self, number, shopName='test', shopKepper='test'):
+        self.basicConfig()
+        self.shopConfig()
+        ChangePwd(self.driver).switch_frame()
         self.pullDown()
-        self.select_shop(platform)
+        sleep(1)
+        self.select_shop(number)
         self.shopName(shopName)
         self.shopKepperName(shopKepper)
         self.saveBtn()
--- crm/test_case/shop_sta.py
+++ crm/test_case/shop_sta.py
@@ -2,6 +2,7 @@ import unittest
 from models import myunit, function
 from models.page_object.shopPage import Shop
 from models.page_object.loginPage import Login
+from models.page_object.changepwdPage import ChangePwd
 from models.driver import *
 from time import *
 
@@ -9,13 +10,16 @@ from time import *
 class ShopNameTest(myunit.MyTest):
     '''设置店铺信息测试'''
 
-    def shopName_verify(self, platform, shopName, shopKepper):
-        shopSetting(self.driver).shopSetting(platform, shopName, shopKepper)
+    def shopName_verify(self, number, shopName='', shopKepper=''):
+        Shop(self.driver).shopSetting(number, shopName, shopKepper)
 
     def test_setting_1(self):
+        '''
+        平台为淘宝 其他信息为空
+        '''
         Login(self.driver).user_login()
-        A = Shop.select_shop(0)
-        self.shopName_verify(A, 'aaa', 'qqq')
+        self.shopName_verify(0)
+        sleep(5)
 
 
 if __name__ == '__main__':
